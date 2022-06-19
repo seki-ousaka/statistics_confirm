fe3144a42c4d3a386062b40eca190286997f1870
seki <>
2022-06-19 17:57:36 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use statistics_confirm::mean::*;
+use statistics_confirm::median::*;
+use statistics_confirm::mode::*;
+
+fn main() {
+
+    mean::mean();
+
+    median::median();
+
+    mode::mode();
+}
